20c27f5e98af67b8a93f1857bb95bc2dfc9f94c2
junhyeok928 <junhyeok928@naver.com>
2022-01-12 18:03:13 +0900

    회원가입 DB 구현

--- src/main/java/DAO/member_DAO.java
+++ src/main/java/DAO/member_DAO.java
@@ -6,85 +6,119 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import VO.member_VO;
+
 public class member_DAO {
-		private Connection con;
-		private PreparedStatement pstmt;
-		private ResultSet rs;
-		
+	private Connection con;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 
-		public void setConn() throws SQLException {
+	public void setConn() throws SQLException {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("드라이버 예외:" + e.getMessage());
+		}
+		String info = "jdbc:oracle:thin:@localhost:1521:xe";
+		con = DriverManager.getConnection(info, "scott", "tiger");
+		System.out.println("DB 접속 성공!!");
+	}
+
+	void closeRsc() {
+		if (rs != null) {
 			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
-			} catch (ClassNotFoundException e) {
+				rs.close();
+			} catch (SQLException e1) {
 				// TODO Auto-generated catch block
-				System.out.println("드라이버 예외:" + e.getMessage());
+				e1.printStackTrace();
 			}
-			String info = "jdbc:oracle:thin:@localhost:1521:xe";
-			con = DriverManager.getConnection(info, "scott", "tiger");
-			System.out.println("DB 접속 성공!!");
 		}
-		void closeRsc() {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+		}
+
+	}
+
+	public void insertMember(member_VO member) {
+		try {
+			setConn();
+			String sql = "insert into Users \r\n"
+					+ "values((SELECT TO_NUMBER(NVL(MAX(USER_NO), '0')) + 1 FROM USERS u )\r\n"
+					+ ",?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, member.getId());
+			pstmt.setString(2, member.getPw());
+			pstmt.setString(3, member.getName());
+			pstmt.setString(4, (member.getPhone1() + "-" + member.getPhone2() + "-" + member.getPhone3()));
+			pstmt.setString(5, member.getEmail());
+			pstmt.executeQuery();
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("DB관련 예외:" + e.getMessage());
+			closeRsc();
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				// TODO: handle exception
+				e1.printStackTrace();
 			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("일반 예외:" + e.getMessage());
 
 		}
-		public int loginCheck(String id, String pw) {
-			int x = -1;
+	}
+
+	public int loginCheck(String id, String pw) {
+		int x = -1;
+		try {
+			setConn();
+			con.setAutoCommit(false);
+			String sql = "select * from Users where user_id = ? and user_pw = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			pstmt.executeQuery();
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				x = 1;
+			} else {
+				x = 0;
+			}
+			pstmt.close();
+			con.close();
+
+		} catch (SQLException e) {
+			System.out.println("DB관련 예외:" + e.getMessage());
+			closeRsc();
 			try {
-				setConn();
-				con.setAutoCommit(false);
-				String sql = "select * from Users where user_id = ? and user_pw = ?";
-				pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, id);
-				pstmt.setString(2, pw);
-				pstmt.executeQuery();
-				rs = pstmt.executeQuery();
-				if(rs.next()) {
-					x = 1;
-				}else {
-					x = 0;
-				}
-				pstmt.close();
-				con.close();
-						
-			}catch(SQLException e) {
-				System.out.println("DB관련 예외:" + e.getMessage());
-				closeRsc();
-				try {
-					con.rollback();
-				} catch (SQLException e1) {
-					// TODO: handle exception
-					e1.printStackTrace();
-				}
-			}
-			catch (Exception e) {
+				con.rollback();
+			} catch (SQLException e1) {
 				// TODO: handle exception
-				System.out.println("일반 예외:" + e.getMessage());
-				
+				e1.printStackTrace();
 			}
-			return x;
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("일반 예외:" + e.getMessage());
+
 		}
+		return x;
+	}
 
 }
--- src/main/webapp/전/loginpro.jsp
+++ src/main/webapp/전/loginpro.jsp
@@ -1,23 +1,17 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"
-    import="java.util.*"
-    import="jspexp.z01_vo.*"
-    import="jspexp.a03_database.*"  
-    import="DAO.member_DAO"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>    
-<fmt:requestEncoding value="UTF-8" /> 
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" import="java.util.*" import="jspexp.z01_vo.*" import="jspexp.a03_database.*" import="DAO.member_DAO"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<fmt:requestEncoding value="UTF-8" />
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 
-<script type="text/javascript" 
-  src="${path}/a00_com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="${path}/a00_com/jquery-3.5.1.js"></script>
 </head>
 <body>
-<%
+	<%
 	request.setCharacterEncoding("euc-kr");
 	String id = request.getParameter("userId");
 	String pw = request.getParameter("userPw");
@@ -25,9 +19,8 @@
 	member_DAO dao = new member_DAO();
 	int check = dao.loginCheck(id, pw);
 	String msg = "";
-	
-	if (check == 1)
-	{
+
+	if (check == 1) {
 		session.setAttribute("sessionID", id);
 		msg = "./main.jsp";
 	} else {
